d9b5a8d5e0ad6d6f65975dbfe583dd87b202a7f6
Milton Ibarra <elmiltonibarra@gmail.com>
2026-01-30 22:19:05 -0300

    Update Prisma configuration and schema to use username field and correct database URL for user registration

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 import { PrismaClient } from "../lib/generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
-const connectionString = `${process.env.DATABASE_URL}`;
+const connectionString = `${process.env.DIRECT_URL}`;
 
 const adapter = new PrismaPg({ connectionString });
 const prisma = new PrismaClient({ adapter });
